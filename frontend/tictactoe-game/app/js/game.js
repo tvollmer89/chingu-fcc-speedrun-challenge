f1a8938290e6e111710098c05386d81c9b8109ea
Theresa <tvollmer89@gmail.com>
2017-05-10 12:24:51 -0500

    Initial commit

---
+++ frontend/tictactoe-game/app/js/game.js
@@ -0,0 +1,22 @@
+/**
+
+  TODO:
+  - startGame function to set player choice
+  - remove temporary
+ */
+
+/**
+ *  Function to set player token (either X or O)
+ *  @param  {string} p player's choice
+ *  @return {void}   starts the game
+ */
+function startGame(c) {
+
+}
+
+var game = {
+  user: '',
+  computer: '',
+  currentPlayer: '',
+  moves: 1,
+};
